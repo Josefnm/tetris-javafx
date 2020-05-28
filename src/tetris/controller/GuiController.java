58e109bf46e68c00fe91b754fbbd57c40b250b16
josef <josefnm86@gmail.com>
2020-05-28 19:37:31 +0200

    Implemented basic controller for gui

---
+++ src/tetris/controller/GuiController.java
@@ -0,0 +1,25 @@
+package tetris.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Label;
+
+public class GuiController {
+    @FXML
+    private Label gameOverLabel;
+    @FXML
+    private Label levelLabel;
+    @FXML
+    private Label clearedLabel;
+    @FXML
+    private Label scoreLabel;
+    @FXML
+    private Canvas gameCanvas;
+
+    @FXML
+    public void initialize() {
+        //TODO replace with constants
+        gameCanvas.setHeight(600);
+        gameCanvas.setWidth(300);
+    }
+}
