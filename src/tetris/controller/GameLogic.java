9380200ae53be36b8db6ed202b1d75cd109d660f
josef <josefnm86@gmail.com>
2020-05-29 02:57:34 +0200

    added game board

--- src/tetris/controller/GameLogic.java
+++ src/tetris/controller/GameLogic.java
@@ -1,10 +1,17 @@
 package tetris.controller;
 
 import javafx.animation.AnimationTimer;
+import javafx.scene.paint.Color;
 
 public class GameLogic {
 
     private long frame = 0;
+
+    private Color[][] gameGrid;
+
+    public GameLogic() {
+        this.gameGrid = new Color[10][20];
+    }
 
     public void runTimer() {
         AnimationTimer animationTimer = new AnimationTimer() {
